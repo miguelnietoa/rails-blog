87f13309cbbcdd7ade448962a496ed13919f6fb1
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-10-19 08:53:44 -0500

    CRUD operations (#3)
    * Show a Single Article
    
    * Resourceful Routing
    
    * Create a new article
    
    Using a form builder
    
    Using strong parameters
    
    Validate inputs and display error messages
    
    Link page to create new articles from the bottom of index page
    
    * Update an article
    
    Using Partials to Share View Code
    
    Link to the edit page from the show page
    
    * Delete an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,48 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(articles_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+  def articles_params
+    params.require(:article).permit(:title, :body)
+  end
 end
