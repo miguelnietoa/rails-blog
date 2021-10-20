5e88635c47c59a7e60d7cfd7c4ed11acdb0c3759
Miguel Nieto A <anietom@uninorte.edu.co>
2021-10-20 09:27:13 -0500

    Format and correct code with Rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
-  
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-  
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
+
   def index
     @articles = Article.all
   end
