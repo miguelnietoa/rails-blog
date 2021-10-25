01a9300c708af8eaca7594ca9c20185ed16c7eef
Miguel Nieto A <anietom@uninorte.edu.co>
2021-10-25 16:11:06 -0500

    fix: typo calling a method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
 
-    if @article.update(articles_params)
+    if @article.update(article_params)
       redirect_to @article
     else
       render :edit
