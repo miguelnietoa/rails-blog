7cf9d4d5c7fa5d55ca6c932abbc9d463b7756451
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2021-10-28 15:32:56 -0500

    Followers feature (#26)
    * chore: create Follow model and associations
    
    * feat: set up routes and create actions
    
    * chore: migrate db
    
    * feat: follow and unfollow
    
    * feat: add comments only if user's following article author
    
    * feat: Improve styles
    
    Same styles for buttons
    Add icons to some buttons
    
    * fix: dont show button when author blog is current user
    
    * chore: set up route to show user profile
    
    * feat: user profile
    
    - Create partial for article item
    - Add article item to user profile
    
    * chore: Update README.md
    
    * feat: link username labels to redirect to user profile
    
    * feat: Show following and followers count

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followees, through: :followed_users
+  has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
+  has_many :followers, through: :following_users
+
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: { case_sensitive: true }
+  validates :password_digest, presence: true
+  validates :info, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :follows
   root 'articles#index'
 
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, except: [:new]
+  resources :users, except: %i[new show]
+  get '/:username', to: 'users#show', as: 'show_user'
+  post '/users/:id/follow', to: 'users#follow', as: 'follow_user'
+  post '/users/:id/unfollow', to: 'users#unfollow', as: 'unfollow_user'
 
   resources :articles do
     resources :comments
